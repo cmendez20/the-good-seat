8ee21c4f1322442d2a4d3310fb895c4bdf4b54a9
chris mendez mba <mendecj812@gmail.com>
2025-06-15 16:30:14 -0500

    feat: add review type

--- client/app/types/types.ts
+++ client/app/types/types.ts
@@ -10,3 +10,14 @@ export type Theatre = {
   city: string;
   state: string;
 };
+
+export type Review = {
+  screenId: number;
+  screenName: string;
+  screenType: string;
+  notes: string;
+  liked: string;
+  seatRow: string;
+  seatNum: number;
+  timestamp: string;
+};
