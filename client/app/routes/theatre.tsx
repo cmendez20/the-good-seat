757cc433b54b5752002ae52f75c76802132c358f
chris mendez mba <mendecj812@gmail.com>
2025-06-15 16:32:49 -0500

    feat: make theatre page slightly more pretty

--- client/app/routes/theatre.tsx
+++ client/app/routes/theatre.tsx
@@ -1,6 +1,7 @@
 import { ReviewCard } from "~/components/reviews/ReviewCard";
 import type { Route } from "./+types/theatre";
-import type { Theatre } from "~/types/types";
+import type { Review, Theatre } from "~/types/types";
+import { Link } from "react-router";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -17,19 +18,24 @@ export async function clientLoader({ params }: Route.ClientLoaderArgs) {
 export default function Theatre({ loaderData }: Route.ComponentProps) {
   const { theaterInfo, reviews } = loaderData;
   return (
-    <div className="max-w-2xl m-auto pt-8">
+    <div className="max-w-sm m-auto py-8 px-4">
       <div className="mb-6">
         <h1 className="text-2xl">{theaterInfo.name}</h1>
         <p className="text-sm italic">
           {theaterInfo.city}, {theaterInfo.state}
         </p>
       </div>
-
       <div>
-        {reviews.map((review, i) => (
+        {reviews.map((review: Review, i: number) => (
           <ReviewCard review={review} key={i} />
         ))}
       </div>
+      <Link
+        to={"/"}
+        className="transition-colors hover:text-white hover:underline"
+      >
+        &larr; Home
+      </Link>
     </div>
   );
 }
