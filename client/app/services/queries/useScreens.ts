724c3b84693c2f352755c208328a35e8f360a3fe
chris mendez mba <mendecj812@gmail.com>
2025-06-15 16:35:34 -0500

    refactor: remove tanstack query

--- client/app/services/queries/useScreens.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getScreens } from "../api";
-
-export function useScreens() {
-  return useQuery({
-    queryKey: ["screens"], // A unique key for this query
-    queryFn: getScreens, // The function that will fetch the data
-  });
-}
--- client/app/services/queries/useTheatres.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTheatres } from "../api";
-
-export function useTheatres() {
-  return useQuery({
-    queryKey: ["theatres"], // A unique key for this query
-    queryFn: getTheatres, // The function that will fetch the data
-  });
-}
